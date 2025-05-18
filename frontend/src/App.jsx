7b349359e4aa60a2af17f6617da60b35f29c29f5
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-05-18 14:20:34 +0530

    login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 import AuthPage from './pages/AuthPage'
-import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 
 function App() {
 
   return (
     <>
-    <ProfilePage />
+    <AuthPage/>
     </>
   )
 }
