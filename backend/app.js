75911b70f20f166268b39640e84c3337af4ecdd8
5tarun3 <tarun.devrath@gmail.com>
2025-05-18 15:36:27 +0530

    all changes

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ initDB().catch(err => {
 });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6080;
 
 // Middleware
 app.use(cors({ origin: process.env.CLIENT_URL }));
