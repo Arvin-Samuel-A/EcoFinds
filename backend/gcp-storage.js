8ff6028160e54f9fa931234cbd4c99592aac60a3
5tarun3 <tarun.devrath@gmail.com>
2025-06-04 22:52:43 +0530

    updates

--- backend/gcp-storage.js
+++ backend/gcp-storage.js
@@ -5,7 +5,7 @@ import path from 'path';
 // Initialize Google Cloud Storage
 const storage = new Storage({
   projectId: process.env.GCP_PROJECT_ID,
-  keyFilename: process.env.GCP_KEY_FILE, // Path to your service account key file
+  keyFilename: process.env.GCP_KEY_FILE,
 });
 
 const bucket = storage.bucket(process.env.GCP_BUCKET_NAME);
